cb34c355abe8aed824911755d8cc56bc00a7c299
Beurredepin <vincent.boisvert04@hotmail.com>
2026-05-11 20:19:06 -0400

    flatlist beginning, missing rest of index and listItemCard component

--- pfi/app/(tabs)/products/index.tsx
+++ pfi/app/(tabs)/products/index.tsx
@@ -1,25 +1,31 @@
+import ListProductCard from '@/components/ListProductCard';
 import Colors from '@/constants/Colors';
 import { typography } from '@/constants/typography';
 import { useSQLiteContext } from 'expo-sqlite';
-import { useEffect } from 'react';
-import { Text, useColorScheme, View } from 'react-native';
-
-const getProducts = async () => {
-    const db = useSQLiteContext();
-    const produits = await db.getAllAsync<{ id: number, nom: string, description: string, prix: number, image: string }>("SELECT * FROM produits"); // pour obtenir tous les résultats sous forme de tableau d'objets.
-    return produits.map((e: { id: number, nom: string, description: string, prix: number, image: string }) => { e });
-}
+import { useEffect, useState } from 'react';
+import { FlatList, Text, useColorScheme, View } from 'react-native';
 
 export default function Products() {
     const colorScheme = useColorScheme()
     const colors = Colors[colorScheme ?? 'light'];
+    const db = useSQLiteContext();
+    const [produits, setProduits] = useState<any[]>([]);
     useEffect(() => {
-        const produits = getProducts;
-    });
+        //prendre les produits de la bd
+        async function getProducts() {
+            const produits = await db.getAllAsync("SELECT * FROM produits"); // pour obtenir tous les résultats sous forme de tableau d'objets.
+            setProduits(produits);
+        }
+        //appeler la fonction
+        getProducts();
+    },[]);
     return (
         <View>
             <Text style={[typography.title, { color: colors.text }]}>Produits</Text>
-            for(const produit of tabProduits)
+            <FlatList
+                data={produits}
+                renderItem={(item:any) => ListProductCard(item)}
+                keyExtractor={item => item.id} />
         </View>
     )
 }
